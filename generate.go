c417362a66a846dd02c8b7fe2e0d6418e60f9c16
Nobuo Kawaguchi <kawaguti@nagoya-u.jp>
2020-01-05 14:17:16 +0900

    Update generate.go

--- generate.go
+++ generate.go
@@ -1,4 +1,4 @@
-package proto_geographic
+package proto_geography
 
 // dummy go file for generating pb.go
-//go:generate protoc -I . --go_out=paths=source_relative:. geographic.proto
+//go:generate protoc -I . --go_out=paths=source_relative:. geography.proto
